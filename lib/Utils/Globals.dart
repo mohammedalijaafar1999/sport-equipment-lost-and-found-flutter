d4e7995b7afc20969a39614c0cb5b431d31a3362
Mohammed Ali Haafar <mohammedalijaafar1999@gmail.com>
2021-12-30 09:30:24 +0300

    now changed

--- lib/Utils/Globals.dart
+++ lib/Utils/Globals.dart
@@ -1,3 +1,3 @@
-String hostname = "http://192.168.100.14:8000";
+// String hostname = "http://192.168.100.14:8000";
 // String hostname = "http://project.ghareeb.co";
-// String hostname = "https://project.ghareeb.co";
+String hostname = "https://project.ghareeb.co";
